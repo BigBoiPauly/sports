5dffce4fb58fce18674781f94a66e7b6e6b98bcf
Paul Carnahan <Paul@Lauras-MacBook-Air.local>
2021-03-13 17:22:22 -0700

    started nba files

---
+++ main.py
@@ -0,0 +1,5 @@
+import nba_api
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.static import teams 
+from nba_api.stats.endpoints import leaguegamefinder
